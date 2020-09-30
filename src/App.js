081d521ace241d30118b1da7b652214ad5315e2a
Zac Smith <zrs3141592@gmail.com>
2020-09-30 15:02:38 -0600

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import HomeScreen from './screens/HomeScreen.jsx'
+import ProductScreen from './screens/ProductScreen.jsx'
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
-    </>
+    <Router>
+      <>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </>
+    </Router>
   )
 }
 
