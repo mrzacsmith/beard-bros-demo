cbf784b908b1de15c15998601a90f9b760a08263
Zac Smith <zrs3141592@gmail.com>
2020-09-30 12:49:49 -0600

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
   return (
     <>
-      <h1>Beard Bros</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Beard Bros</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
