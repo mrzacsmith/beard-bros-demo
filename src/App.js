1de9d8234a1ccecd4608a96ce2c41e185148d04d
Zac Smith <zrs3141592@gmail.com>
2020-09-30 10:45:26 -0600

    initial scaffold

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import './App.css'
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* Remove everything from this line to line 37 */}
+      <h2>Welcome Lambda student! This is filler, no need to keep it!</h2>
+      <a href="https://lambdaschool.com/" target="_blank" rel="noopener noreferrer">
+        Visit the Lambda Llama home
+      </a>
+      <br />
+      <img
+        alt="Lambda loves our llamas!"
+        style={{ width: 'auto', height: '300px' }}
+        src="https://pbs.twimg.com/media/Egh41YNU4AADnsM.png"
+      ></img>
+      <div>
+        <p>Created using Create-React-App</p>
+        <p>Modified and maintained by Lambda School graduate:</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Zac Smith {' '}
+          <a href="https://github.com/mrzacsmith" target="_blank" rel="noopener noreferrer">
+            Github
+          </a>{' '}
+          ::{' '}
+          <a href="https://www.npmjs.com/~mrzacsmith" target="_blank" rel="noopener noreferrer">
+            NPM
+          </a>{' '}
+          ::{' '}
+          <a href="https://twitter.com/mrzacsmith" target="_blank" rel="noopener noreferrer">
+            Twitter
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      {/* Remove everything from line 8 to here */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
